db96526d6d30e163cc287a8a0970829e52748ce5
ADITYA KUMAR SINGH <54054645+autosaver@users.noreply.github.com>
2022-02-07 16:58:49 +0530

    env fault

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [data, setdata] = useState([]);
 
 
-  let url = process.env.URL
+  let url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&key=AIzaSyAD9l9pGCgVlpm2RaRErvWv4TdwZVdiCYA&q="
   let video_url = "https://www.y2mate.com/youtube/";
   useEffect(async () => {
     await fetch(url+"coding").then((res) => res.json()).then((res) => setdata(res.items));
